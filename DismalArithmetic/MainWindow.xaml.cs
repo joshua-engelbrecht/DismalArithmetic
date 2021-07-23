32fd2818a783c5efc91f6131c0295487bfcd687b
Joshua Engelbrecht <jengelbrecht@pksti.com>
2021-07-23 16:01:04 -0500

    initial commit

--- DismalArithmetic/MainWindow.xaml.cs
+++ DismalArithmetic/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,28 @@ namespace DismalArithmetic
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string OperationImage { get; set; } 
         public MainWindow()
         {
             InitializeComponent();
+
+            OperationImage = "/DismalArithmetic;compoent/Images/plus.png";
+        }
+
+        private void PreviewTextInputValidate(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        private static readonly Regex _regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
+        private static bool IsTextAllowed(string text)
+        {
+            return !_regex.IsMatch(text);
+        }
+
+        private void ChangeOperation(object sender, MouseButtonEventArgs e)
+        {
+            OperationImage =  OperationImage.Contains("plus.png") ? "/DismalArithmetic;compoent/Images/multiply.png" : "/DismalArithmetic;compoent/Images/plus.png";
         }
     }
 }
